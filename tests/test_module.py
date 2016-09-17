869a420b3cebb30ad4f301b61305db4ee70d95b0
Tomasz Los <tloszabno@gmail.com>
2016-09-17 20:58:40 +0200

    Added notification deps + tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,21 @@
 Test used only to check if all necessary libs are available
 """
 import unittest
+import time
 
 from tl_en import tl_en
 from hamcrest import *
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
 
 class TestModule(unittest.TestCase):
-    def TestSum(self):
+    def testSum(self):
         assert_that(tl_en.sum(1,3), equal_to(4))
-        print("Module structure ok, libs imported")
+
+    def testNotifications(self):
+        Notify.init("Hello world")
+        Hello=Notify.Notification.new("Hello world", "This is an example notification.", "dialog-information")
+        Hello.show()
+        time.sleep(1)
+        Hello.close()
